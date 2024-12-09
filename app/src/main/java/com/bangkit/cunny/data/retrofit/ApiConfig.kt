4ed17fe46cf1337a9e40c33ce67ea36820d974cf
Nagami <117897427+Naived@users.noreply.github.com>
2024-12-09 15:10:10 +0700

    Update ApiConfig.kt

--- app/src/main/java/com/bangkit/cunny/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/bangkit/cunny/data/retrofit/ApiConfig.kt
@@ -14,7 +14,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://cunny-api-66.et.r.appspot.com/")
+                .baseUrl(key=api)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
